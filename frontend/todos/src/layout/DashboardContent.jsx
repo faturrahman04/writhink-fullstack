0718c305af69b68dc4c9b887f13cd3532b570100
faturrahman04 <faturrahmanalfarisi449@gmail.com>
2025-09-12 22:54:28 +0700

    feat(button, modal): menambahkan fitur tombol untuk tambah tugas dan menampilkan modal box

--- frontend/todos/src/layout/DashboardContent.jsx
+++ frontend/todos/src/layout/DashboardContent.jsx
@@ -1,13 +1,27 @@
+import useStoreModal from "../hooks/useStoreModal"
+import Button from "../components/Button"
 import InputSearch from "../components/InputSearch"
 import Navbar from "../components/Navbar"
 import Todos from "../components/Todos"
 
+
 const DashboardContent = () => {
+  const { modal, setModal } = useStoreModal()
+
+  function handleModal(e) {
+    e.preventDefault();
+    setModal(!modal);
+  }
+
+
   return (
     <div className="flex flex-col max-w-full w-full">
       <Navbar />
       <div className="max-w-full w-full px-6 py-8">
-        <h1 className="font-bold text-3xl 2xl:text-4xl">To Do List</h1>
+        <div className="flex justify-between">
+          <h1 className="font-bold text-3xl 2xl:text-4xl">To Do List</h1>
+          <Button onModalClick={handleModal} variant={`bg-blue-500 text-slate-50 hover:bg-blue-400 py-2`}>Tambah Tugas</Button>
+        </div>
         <InputSearch />
 
         {/* Todos */}
