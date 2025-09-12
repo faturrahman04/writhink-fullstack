71e2e71229ff917e52add216e6779b1385736489
faturrahman04 <faturrahmanalfarisi449@gmail.com>
2025-09-12 22:59:32 +0700

    feat(modal): menambahkan modal box

--- frontend/todos/src/pages/Dashboard.jsx
+++ frontend/todos/src/pages/Dashboard.jsx
@@ -1,11 +1,16 @@
+import ModalForm from "../components/ModalForm";
 import Sidebar from "../components/Sidebar";
 import DashboardContent from "../layout/DashboardContent";
 
 const Dashboard = () => {
+
   return (
     <div className="flex">
       <Sidebar />
       <DashboardContent />
+
+      <ModalForm />
+
     </div>
   )
 }
