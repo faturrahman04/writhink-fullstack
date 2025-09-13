31299e045b2b604e0cf20fd137dacc81fac35765
faturrahman04 <faturrahmanalfarisi449@gmail.com>
2025-09-13 18:51:15 +0700

    style(position): Memperbarui sifat pada judul konten dashboard agar fix diatas

--- frontend/todos/src/layout/DashboardContent.jsx
+++ frontend/todos/src/layout/DashboardContent.jsx
@@ -17,12 +17,17 @@ const DashboardContent = () => {
   return (
     <div className="flex flex-col max-w-full w-full">
       <Navbar />
-      <div className="max-w-full w-full px-6 py-8">
-        <div className="flex justify-between">
-          <h1 className="font-bold text-3xl 2xl:text-4xl">To Do List</h1>
-          <Button onModalClick={handleModal} variant={`bg-blue-500 text-slate-50 hover:bg-blue-400 py-2`}>Tambah Tugas</Button>
+      <div className="max-w-full w-full px-6">
+        <div className="flex flex-col sticky top-16 z-20 bg-slate-100 py-8">
+
+          <div className="flex justify-between">
+            <h1 className="font-bold text-3xl 2xl:text-4xl">To Do List</h1>
+            <Button onModalClick={handleModal} variant={`bg-blue-500 text-slate-50 hover:bg-blue-400 py-2`}>Tambah Tugas</Button>
+          </div>
+
+            <InputSearch />
+
         </div>
-        <InputSearch />
 
         {/* Todos */}
           <Todos />
