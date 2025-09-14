63f2f1ccee1a0dd478cba5928aeb40d920aff6b3
faturrahman04 <faturrahmanalfarisi449@gmail.com>
2025-09-15 02:02:47 +0700

    style(Navbar): Menambahkan navbar pada landing page

--- frontend/todos/src/App.jsx
+++ frontend/todos/src/App.jsx
@@ -1,8 +1,9 @@
-import Button from "./components/Button"
+import LandingNavbar from "./components/LandingNavbar"
 
 const App = () => {
   return (
     <>
+      <LandingNavbar />
       <h1>Hello</h1>
       <a href="/dashboard">Dashboard</a>
     </>
