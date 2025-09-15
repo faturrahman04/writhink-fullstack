78220d072666f779b81975fdba42c221ce82ddef
faturrahman04 <faturrahmanalfarisi449@gmail.com>
2025-09-15 21:45:00 +0700

    fixed(validasi): Memperbaiki dan menambahkan validasi untuk expired token apabila lebih dari 1 jam

--- frontend/todos/src/components/ProtectDashboard.jsx
+++ frontend/todos/src/components/ProtectDashboard.jsx
@@ -1,7 +1,16 @@
 import { Navigate } from "react-router";
+import { jwtDecode } from "jwt-decode";
 
 const ProtectDashboard = ({children}) => {
   const token = localStorage.getItem('token');
+
+  const decoded = jwtDecode(token);
+  const expiredToken = decoded.exp * 1000;
+  
+  if (Date.now() > expiredToken) {
+    localStorage.removeItem('token')
+  }
+
   if (!token) {
     return <Navigate to="/login" replace />
   }
