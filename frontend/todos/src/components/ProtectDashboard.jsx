1d906d02b4b811a6498858e4730dd0b7521b86f0
faturrahman04 <faturrahmanalfarisi449@gmail.com>
2025-09-16 21:17:09 +0700

    fixed(auth): Memperbaiki validasi auth saat user telah kehabisan token

--- frontend/todos/src/components/ProtectDashboard.jsx
+++ frontend/todos/src/components/ProtectDashboard.jsx
@@ -4,16 +4,24 @@ import { jwtDecode } from "jwt-decode";
 const ProtectDashboard = ({children}) => {
   const token = localStorage.getItem('token');
 
-  const decoded = jwtDecode(token);
-  const expiredToken = decoded.exp * 1000;
-  
-  if (Date.now() > expiredToken) {
-    localStorage.removeItem('token')
-  }
-
   if (!token) {
     return <Navigate to="/login" replace />
   }
+
+  try {
+    const decoded = jwtDecode(token);
+    const expiredToken = decoded.exp * 1000;
+    
+    if (Date.now() > expiredToken) {
+      localStorage.removeItem('token')
+      return <Navigate to="/login" replace />
+    }
+  } catch (err) {
+    console.log(err)
+    localStorage.removeItem('token');
+    return <Navigate to="/login" replace />
+  }
+
   return children;
 }
 
