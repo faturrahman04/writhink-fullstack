16367d4bce874f26d6167b655cf6e629cf0e9108
faturrahman04 <faturrahmanalfarisi449@gmail.com>
2025-09-27 11:10:19 +0700

    skip ssl pt2

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,7 @@ const db = mysql.createConnection({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+  charset: 'utf8mb4',
   // ssl : {
   //   ca: fs.readFileSync(join(process.cwd(), 'cert', 'ca.pem')), // ganti saat dev
   //   rejectUnauthorized: true
